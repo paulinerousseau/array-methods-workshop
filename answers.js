7fe40e3cc095e5aee4b3b1fc053d4e316a1597eb
pauline <pauline_rousseau@hotmail.co.uk>
2016-07-07 18:44:19 +0000

    printpositives

---
+++ answers.js
@@ -0,0 +1,12 @@
+
+// printPositives
+var myNumArr = [1,-2,3,-6];
+
+function printPositives(num){
+    if (num>0){
+        console.log(num)
+    }
+}
+   myNumArr.forEach(printPositives);
+   
+       
